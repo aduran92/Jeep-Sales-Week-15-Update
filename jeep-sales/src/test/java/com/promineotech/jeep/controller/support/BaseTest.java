0c80d511d5b93a01fa9c4fc12d920e3ee4a8dbc6
Allie Duran <108034603+aduran92@users.noreply.github.com>
2022-10-15 21:33:50 -0600

    Jeep Sales Spring Boot

---
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
@@ -0,0 +1,36 @@
+package com.promineotech.jeep.controller.support;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import lombok.Getter;
+
+public class BaseTest {
+	@LocalServerPort
+	private int serverPort;
+	
+	@Autowired
+	@Getter
+	private TestRestTemplate restTemplate;
+	
+	protected String getBaseUri() {
+		return String.format("http://localhost:%d/jeeps", getServerPort());
+	}
+
+	public TestRestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+	public void setRestTemplate(TestRestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public int getServerPort() {
+		return serverPort;
+	}
+
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
+}
